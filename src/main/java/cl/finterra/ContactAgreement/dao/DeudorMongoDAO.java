9aecc8cc58fbc0735478d6c6b51ca6e34f9d6aa5
jorge7063 <150048568+jorge1101@users.noreply.github.com>
2023-12-27 12:16:53 -0300

    update
    actualizacion datos 2

--- src/main/java/cl/finterra/ContactAgreement/dao/DeudorMongoDAO.java
+++ src/main/java/cl/finterra/ContactAgreement/dao/DeudorMongoDAO.java
@@ -3,7 +3,6 @@ package cl.finterra.ContactAgreement.dao;
 import java.util.List;
 import java.util.Optional;
 
-import cl.finterra.ContactAgreement.rest.ListarRest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import cl.finterra.ContactAgreement.entity.Deudor;
--- src/main/java/cl/finterra/ContactAgreement/rest/UsuarioRest.java
+++ src/main/java/cl/finterra/ContactAgreement/rest/UsuarioRest.java
@@ -1,11 +1,13 @@
 package cl.finterra.ContactAgreement.rest;
 
 import cl.finterra.ContactAgreement.Security.JwtTokenProvider;
+import cl.finterra.ContactAgreement.dao.UsuarioMongoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import cl.finterra.ContactAgreement.controller.UsuarioController;
@@ -19,6 +21,9 @@ public class UsuarioRest {
 
 	@Autowired
 	UsuarioController userController;
+
+	@Autowired
+	private UsuarioMongoDAO userDao;
 
 	private final JwtTokenProvider tokenProvider;
 
@@ -61,6 +66,79 @@ public class UsuarioRest {
 
 
 
+	private void validatePassword(String password, BindingResult result) {
+		// Realizar tus validaciones personalizadas aquí
+		// Ejemplo: al menos 4 números, un punto y una mayúscula
+		// Simplemente como ejemplo, ajusta según tus requisitos
+		if (!password.matches("(.*[0-9]){4,}.*") || !password.contains(".") || !password.matches(".*[A-Z].*")) {
+			result.addError(new FieldError("Usuario", "password", "La contraseña no cumple con los requisitos mínimos"));
+		}
+	}
+
+	// Método para obtener detalles detallados de errores de validación
+	private String getValidationErrorDetails(BindingResult result) {
+		StringBuilder errorMessage = new StringBuilder("Errores de validación: ");
+		for (FieldError error : result.getFieldErrors()) {
+			errorMessage.append(error.getField())
+					.append(" - ")
+					.append(error.getDefaultMessage())
+					.append("; ");
+		}
+		return errorMessage.toString();
+	}
+	@PostMapping("/encontrar-usuario")
+	public ResponseEntity<?> buscarUsuarioPorEmail(@RequestBody String email) {
+		// Obtener el usuario por correo electrónico
+		System.out.println(email.getClass().getSimpleName());
+		Usuario usuarioEncontrado = userDao.findByEmail(email).orElse(null);
+		System.out.println(usuarioEncontrado);
+		// Verificar si el usuario con el correo electrónico especificado existe
+		if (!userDao.existsByEmail(email)) {
+			return new ResponseEntity<>("Usuario no encontrado", HttpStatus.NOT_FOUND);
+		}
+
+
+		if (usuarioEncontrado != null) {
+			return new ResponseEntity<>(usuarioEncontrado, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/{email}/actualizar-contrasena")
+	public ResponseEntity<?> actualizarContrasena(
+			@PathVariable String email,
+			@Validated @RequestBody Usuario usuarioConNuevaContrasena,
+			BindingResult result) {
+
+		// Verificar si el usuario con el correo electrónico especificado existe
+		if (!userDao.existsByEmail(email)) {
+			return new ResponseEntity<>("Usuario no encontrado", HttpStatus.NOT_FOUND);
+		}
+
+		// Realizar validaciones personalizadas
+		validatePassword(usuarioConNuevaContrasena.getPassword(), result);
+
+		// Verificar si hay errores de validación
+		if (result.hasErrors()) {
+			return ResponseEntity.badRequest().body(getValidationErrorDetails(result));
+		}
+
+		// Obtener el usuario existente por correo electrónico
+		Usuario usuarioExistente = userDao.findByEmail(email).orElse(null);
+
+		if (usuarioExistente != null) {
+			// Actualizar la contraseña
+			usuarioExistente.setPassword(usuarioConNuevaContrasena.getPassword());
+
+			// Guardar el usuario actualizado
+			Usuario usuarioActualizado = userDao.save(usuarioExistente);
+
+			return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 
 
